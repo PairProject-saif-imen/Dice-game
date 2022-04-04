54ab5b4b2b3146caf0ff658c9c43a4a3645a93b2
Saif-Ayari <saifayari312@gmail.com>
2022-04-04 15:23:35 +0200

    i

---
+++ dice.js
@@ -0,0 +1,24 @@
+console.log("hello")
+
+dice = [
+    { val: 1, image: "/images/dice 1.jpg" },
+    { val: 2, image: "/images/dice 2.jpg" },
+    { val: 3, image: "/images/dice 3.jpg" },
+    { val: 4, image: "/images/dice 4.jpg" },
+    { val: 5, image: "/images/dice 5.jpg" },
+    { val: 6, image: "/images/dice 6.jpg" }
+]
+$("#btn1").click(function(){
+    $("#image1").append(dice[Math.floor(Math.random() * 6)])
+    
+})
+$("#btn 2").click(function(){
+    $("#image2").append(dice[Math.floor(Math.random() * 6)])
+
+})
+
+
+function compar(){
+    if()
+}
+src = dice[0].image
