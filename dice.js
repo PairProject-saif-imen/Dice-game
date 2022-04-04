dc91d5add75eecf84f3e3d241e63fa3e88e7a63e
Saif-Ayari <saifayari312@gmail.com>
2022-04-04 21:36:08 +0200

    new version

--- dice.js
+++ dice.js
@@ -1,24 +1,78 @@
-console.log("hello")
+var playCount = 0, count1 = 0, count2 = 0;
 
-dice = [
-    { val: 1, image: "/images/dice 1.jpg" },
-    { val: 2, image: "/images/dice 2.jpg" },
-    { val: 3, image: "/images/dice 3.jpg" },
-    { val: 4, image: "/images/dice 4.jpg" },
-    { val: 5, image: "/images/dice 5.jpg" },
-    { val: 6, image: "/images/dice 6.jpg" }
-]
-$("#btn1").click(function(){
-    $("#image1").append(dice[Math.floor(Math.random() * 6)])
-    
-})
-$("#btn 2").click(function(){
-    $("#image2").append(dice[Math.floor(Math.random() * 6)])
+function displayDice() {
 
-})
+    var dicePlayer1 = Math.floor(Math.random() * 6) + 1;
+    var dicePlayer2 = Math.floor(Math.random() * 6) + 1;
+    var imageP1 = $("img")[0];
+    var imageP2 = $("img")[1];
+    imageP1.src = "/images/dice" + dicePlayer1 + ".png";
+
+    imageP2.src = "/images/dice" + dicePlayer2 + ".png";
+    var title = document.querySelector("#title");
+
+    if (dicePlayer1 > dicePlayer2) {
+        title.textContent = " PLAYER 1 Wins";
+        title.classList.add("#title");
+        count1++;
+
+    } else if (dicePlayer2 > dicePlayer1) {
+        title.textContent = "PLAYER 2 Wins ";
+        title.classList.add("#title");
+        count2++;
 
 
-function compar(){
-    if()
+    } else {
+        title.textContent = "Twinning ";
+        title.classList.add("#title");
+    }
+    playCount++;
+
+    if (playCount === 5) {
+        if (count1 > count2) {
+            title.textContent = "Round Winner: PLAYER 1 is Playing  ";
+            count1 = 0;
+            count2 = 0;
+            playCount = 0;
+        } else if (count1 < count2) {
+            title.textContent = "Round Winner: PLAYER 2 is Playing";
+            count1 = 0;
+            count2 = 0;
+            playCount = 0;
+        } else {
+            title.textContent = " Oh! No Winner let's do it again";
+            count1 = 0;
+            count2 = 0;
+            playCount = 0;
+        }
+    }
 }
-src = dice[0].image
+var players = {
+    user1: "",
+    user2: ""
+}
+
+var player1 = $("<h5 id ='u1'></h5>")
+$("#player1").append(u1);
+var player2 = $("<h5 id ='u2'></h5>")
+$("#player2").append(u2);
+function savePlayer1(e) {
+    players.user1 = e.target.value;
+}
+function savePlayer2(e) {
+    players.user2 = e.target.value;
+}
+
+
+$(document).ready(function () {
+    $("#S").click(function (e) {
+        $("#use1").on("change", savePlayer1);
+        $("#use2").on("change", savePlayer2);
+        $("#player1").append(user1);
+        $("#player2").append(user2);
+    })
+
+
+
+
+})
